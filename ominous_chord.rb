acb3a00c2c728339c801c6ca651d2af8b6ffda22
Daniel Szalay <sza.dan.uj@gmail.com>
2025-04-11 12:00:13 +0200

    add ominous chord example

---
+++ ominous_chord.rb
@@ -0,0 +1,4 @@
+play 70, release: 3, attack: 0.5, sustain: 0.5
+play 55, release: 3, attack: 1.5, sustain: 0.5
+play 50, release: 3, attack: 3, sustain: 0.5
+play 30, release: 3, attack: 5, sustain: 0.5
